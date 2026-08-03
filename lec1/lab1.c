20d08ee37c249da3a9021197b91e8443b8822faf
mostafasemary18-code <mostafasemary18@gmail.com>
2026-08-03 04:15:02 +0300

    lec1
    My progress in learning C programming - labs and assignments from my IMT course, updated session by session.

---
+++ lec1/lab1.c
@@ -0,0 +1,12 @@
+// stanrd input output library
+#include <stdio.h>
+/*
+entry point of the program
+this is the main function
+return0
+*/
+
+int main()
+{
+    printf("Hello, World!\n");
+}
---
+++ lec1/lab2.c
@@ -0,0 +1,14 @@
+// stanrd input output library
+#include <stdio.h>
+/*
+entry point of the program
+this is the main function
+return0
+*/
+
+int main()
+{
+printf("my name mostafan");
+	printf("my age is 20");
+	printf("bnu");
+}
