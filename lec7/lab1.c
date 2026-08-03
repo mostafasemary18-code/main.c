960176d94abc352f52f63057358ceb307ef3fcb8
mostafasemary18-code <mostafasemary18@gmail.com>
2026-08-03 04:08:30 +0300

    lec7
    My progress in learning C programming - labs and assignments from my IMT course, updated session by session.

---
+++ lec7/lab1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "STD_TYPES.h"
+int main ()
+{
+	 uint8 x;
+	 uint8 y;
+	 printf("enter your 1st number");
+	 scanf(" %c",&x);
+	  printf("enter your 2st number");
+	   scanf(" %c",&y);
+	 	printf(" %c +%c =%c",x,y,x+y);
+}
